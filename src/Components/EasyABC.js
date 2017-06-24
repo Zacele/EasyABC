ed38a0630868be97abfc6a5b05720878c942a3f8
Khoa Le\admin <le.khoa2209@gmail.com>
2017-06-24 23:48:23 +0700

    Add the Json file and the Guessing Component

--- src/Components/EasyABC.js
+++ src/Components/EasyABC.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
+import alphabets from '../Json/alphabets.json';
 import '../App.css';
 
 class EasyABC extends Component {
+  constructor(props){
+    super(props);
+    this.state= {
+      alphabets: alphabets,
+      currentPosition: 0
+    };
+  }
+
   render(){
     return (
       <div className="game">
         <div className="option">
           <div className="fields">
             <div className="field-block">
-              A
+              {this.state.alphabets[this.state.currentPosition].letter}
             </div>
           </div>
           <div className="buttons">
-            <a href="#" className="button prev">Previous</a>
-            <a href="#" className="button sound">Play Sound Agian</a>
-            <a href="#" className="button next">Next</a>
+            <a className="button prev">Previous</a>
+            <a className="button sound">Play Sound Agian</a>
+            <a className="button next">Next</a>
           </div>
       </div>
     </div>
--- src/Components/Guessing.js
+++ src/Components/Guessing.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
+import alphabets from '../Json/alphabets.json';
 import '../App.css';
 
 class Guessing extends Component {
+  constructor(props){
+    super(props);
+    this.state= {
+      alphabets: alphabets,
+      currentPosition: 0
+    };
+  }
+
   render() {
     return (
       <div className="game">
         <div className="fields">
           <div className="field-block">
-            <div className="left-field">A</div>
-            <div className="right-field">A</div>
+            <div className="left-field">
+                <div className="placeholder-span hide">Click Next to view Image</div>
+                <img src={this.state.alphabets[this.state.currentPosition].image}
+
+                 Alt={this.state.alphabets[this.state.currentPosition].word} />
+            </div>
+            <div className="right-field">
+              <div className="placeholder-span">
+                <div className="word">
+                  {this.state.alphabets[this.state.currentPosition].word.toUpperCase()}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
